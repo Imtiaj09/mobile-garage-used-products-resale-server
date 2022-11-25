c9d1b270fdec921a969056635d7c1250482c98a7
Imtiaj09 <ahmimtiaj@gmail.com>
2022-11-25 23:31:20 +0600

    mongodb has been installed and  conectted successfully

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -7,6 +9,24 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vep38nb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+
+  }
+  finally {
+
+  }
+}
+run().catch(console.log)
+
+
 
 app.get('/', async (req, res) => {
   res.send('Mobile Garage server is running.')
