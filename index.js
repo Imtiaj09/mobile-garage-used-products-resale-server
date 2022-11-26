305caa5ad4490c4755161b5dc1a0ad7a6abd96e5
Imtiaj09 <ahmimtiaj@gmail.com>
2022-11-26 22:54:03 +0600

    JWT token has been created with access_token key

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
+
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -50,6 +52,18 @@ async function run() {
       console.log(booking);
       const result = await bookingsCollection.insertOne(booking);
       res.send(result);
+    });
+
+    //JWT token API
+    app.get('/jwt', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+        return res.send({ accessToken: token })
+      }
+      res.status(403).send({ accessToken: '' })
     });
 
     //All users
